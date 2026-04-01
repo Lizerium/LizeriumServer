2917e62730c01196b81d965fe26e6cf7d88138cf
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-01 13:19:46 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-01

--- src/LizeriumServer/Controllers/HomeController.cs
+++ src/LizeriumServer/Controllers/HomeController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 31 марта 2026 11:07:00
- * Version: 1.0.1
+ * Last Updated: 01 апреля 2026 13:19:33
+ * Version: 1.0.2
  */
 
 using System.Diagnostics;
