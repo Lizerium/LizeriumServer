7857409ce033e79503bde6fd2e285bd121600e5f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-09 11:13:52 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-09

--- src/LizeriumUtilities/FormatsData/AppRequestData/Admin/AjaxData/RequestSaveCommand.cs
+++ src/LizeriumUtilities/FormatsData/AppRequestData/Admin/AjaxData/RequestSaveCommand.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 апреля 2026 14:43:45
- * Version: 1.0.10
+ * Last Updated: 09 апреля 2026 11:13:36
+ * Version: 1.0.11
  */
 
 using System.ComponentModel.DataAnnotations.Schema;
