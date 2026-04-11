80cd21344f0ffe8eda73b071f30b6ca24fb9d854
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-11 13:55:04 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-11

--- src/LizeriumNetSecurity/Middleware/DoSDetectionMiddleware.cs
+++ src/LizeriumNetSecurity/Middleware/DoSDetectionMiddleware.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 апреля 2026 12:48:01
- * Version: 1.0.12
+ * Last Updated: 11 апреля 2026 13:54:51
+ * Version: 1.0.13
  */
 
 using System.Collections.Concurrent;
