d74b45e2c08c416bb06d1693ec5d78d21e226574
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-22 18:58:34 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-22

--- src/LizeriumNetSecurity/Services/SecurityService/IAppSecurityService.cs
+++ src/LizeriumNetSecurity/Services/SecurityService/IAppSecurityService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 апреля 2026 07:08:49
- * Version: 1.0.26
+ * Last Updated: 22 апреля 2026 18:58:18
+ * Version: 1.0.27
  */
 
 namespace LizeriumNetSecurity.Services.SecurityService;
