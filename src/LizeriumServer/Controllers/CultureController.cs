8a0e8131a3b7fd45fc31809c1c9cebf4b6278079
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-25 08:31:40 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-25

--- src/LizeriumServer/Controllers/CultureController.cs
+++ src/LizeriumServer/Controllers/CultureController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 апреля 2026 07:11:28
- * Version: 1.0.29
+ * Last Updated: 25 апреля 2026 08:31:24
+ * Version: 1.0.30
  */
 
 using Microsoft.AspNetCore.Localization;
