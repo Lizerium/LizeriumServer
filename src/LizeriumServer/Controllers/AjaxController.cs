d7eb92e3a8a3bf6dc5dc991ba1a315ebaaeaaae9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-28 14:44:25 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-28

--- src/LizeriumServer/Controllers/AjaxController.cs
+++ src/LizeriumServer/Controllers/AjaxController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 апреля 2026 10:01:53
- * Version: 1.0.32
+ * Last Updated: 28 апреля 2026 14:44:11
+ * Version: 1.0.33
  */
 
 using System.Reflection;
