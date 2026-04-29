452072e98898ffd2990aa1aa385f1d5227db88d3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-29 07:13:22 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-29

--- src/LizeriumServer/FormatsData/AppSeo/BreadcrumbNode.cs
+++ src/LizeriumServer/FormatsData/AppSeo/BreadcrumbNode.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 апреля 2026 14:44:11
- * Version: 1.0.33
+ * Last Updated: 29 апреля 2026 07:13:07
+ * Version: 1.0.34
  */
 
 namespace LizeriumServer.FormatsData.AppSeo
