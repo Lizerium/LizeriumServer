c2a7835227c0bfee9a54603cff6d5258f23590bd
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-04 07:13:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-04

--- src/LizeriumEmail/Accessories/EmailAccessories/EmailExtensions.cs
+++ src/LizeriumEmail/Accessories/EmailAccessories/EmailExtensions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 03 мая 2026 07:12:11
- * Version: 1.0.38
+ * Last Updated: 04 мая 2026 07:13:32
+ * Version: 1.0.39
  */
 
 using LizeriumEmail.FormatsData.AppEmailData;
