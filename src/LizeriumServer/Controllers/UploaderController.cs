3136ecbc7bdd01c72a5d1fb529c282e81af32a07
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-06 10:50:50 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-06

--- src/LizeriumServer/Controllers/UploaderController.cs
+++ src/LizeriumServer/Controllers/UploaderController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 мая 2026 07:21:45
- * Version: 1.0.40
+ * Last Updated: 06 мая 2026 10:50:31
+ * Version: 1.0.41
  */
 
 using LizeriumServer.FormatsData.AppUploaderGameData;
