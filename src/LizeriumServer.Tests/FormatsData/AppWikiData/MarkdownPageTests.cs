679fabef8fdcc0e95f14a1d66fee7bea16b1ccf8
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-12 12:23:04 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-12

--- src/LizeriumServer.Tests/FormatsData/AppWikiData/MarkdownPageTests.cs
+++ src/LizeriumServer.Tests/FormatsData/AppWikiData/MarkdownPageTests.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 мая 2026 10:57:48
- * Version: 1.0.46
+ * Last Updated: 12 мая 2026 12:22:50
+ * Version: 1.0.47
  */
 
 using LizeriumServer.FormatsData.AppWikiData;
