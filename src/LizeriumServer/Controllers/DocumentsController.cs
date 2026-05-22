4461de6389f3bfc1cda14fab33fe484745314777
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-22 12:01:21 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-22

--- src/LizeriumServer/Controllers/DocumentsController.cs
+++ src/LizeriumServer/Controllers/DocumentsController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 мая 2026 11:58:33
- * Version: 1.0.55
+ * Last Updated: 22 мая 2026 12:01:04
+ * Version: 1.0.56
  */
 
 using System.Text.Json;
