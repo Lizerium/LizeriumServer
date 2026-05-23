970cb0465e1b26d6b5f010db527ff50c2f987ca6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-23 11:33:29 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-23

--- src/LizeriumEmail/FormatsData/AppEnumsData/TypeEmail.cs
+++ src/LizeriumEmail/FormatsData/AppEnumsData/TypeEmail.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 22 мая 2026 12:01:04
- * Version: 1.0.56
+ * Last Updated: 23 мая 2026 11:33:14
+ * Version: 1.0.57
  */
 
 namespace LizeriumEmail.FormatsData.AppEnumsData;
