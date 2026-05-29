bbfa4d9174e32ccdde127ad1f67d40456e0f27cd
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-29 14:15:58 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-29

--- src/LizeriumServer/Services/Hosted/SitemapHostedService.cs
+++ src/LizeriumServer/Services/Hosted/SitemapHostedService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 мая 2026 17:50:49
- * Version: 1.0.62
+ * Last Updated: 29 мая 2026 14:15:42
+ * Version: 1.0.63
  */
 
 using LizeriumServer.Services.Breadcrumb;
