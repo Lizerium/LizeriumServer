eae59f878d94f78fda7e598693d5c1c17f2910c6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-31 15:11:12 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-31

--- src/LizeriumServer/Controllers/KnowledgeController.cs
+++ src/LizeriumServer/Controllers/KnowledgeController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 мая 2026 16:01:02
- * Version: 1.0.64
+ * Last Updated: 31 мая 2026 15:10:59
+ * Version: 1.0.65
  */
 
 using System.Text.RegularExpressions;
