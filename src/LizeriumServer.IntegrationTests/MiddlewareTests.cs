8f2761f0c32c8392fab2c233f302658d47040d29
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-08 07:12:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-08

--- src/LizeriumServer.IntegrationTests/MiddlewareTests.cs
+++ src/LizeriumServer.IntegrationTests/MiddlewareTests.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 июня 2026 18:42:26
- * Version: 1.0.72
+ * Last Updated: 08 июня 2026 07:12:33
+ * Version: 1.0.73
  */
 
 using Microsoft.AspNetCore.Mvc.Testing;
