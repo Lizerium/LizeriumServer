d91828264feebe3a4898bb0e547b5c4cb3b19dcc
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-11 13:47:54 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-11

--- src/Api.LizeriumServer/Accessories/FormatAccessories/ApiFormatExtensions.cs
+++ src/Api.LizeriumServer/Accessories/FormatAccessories/ApiFormatExtensions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 июня 2026 14:33:59
- * Version: 1.0.75
+ * Last Updated: 11 июня 2026 13:47:38
+ * Version: 1.0.76
  */
 
 using LizeriumCrypt.Accessories;
