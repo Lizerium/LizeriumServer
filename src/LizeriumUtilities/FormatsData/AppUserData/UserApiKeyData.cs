158fc2ffb21e680a9153750db18a934986c26c1d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-15 07:13:05 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-15

--- src/LizeriumUtilities/FormatsData/AppUserData/UserApiKeyData.cs
+++ src/LizeriumUtilities/FormatsData/AppUserData/UserApiKeyData.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 июня 2026 16:53:24
- * Version: 1.0.79
+ * Last Updated: 15 июня 2026 07:12:48
+ * Version: 1.0.80
  */
 
 namespace LizeriumUtilities.FormatsData.AppUserData;
