9759c54ceb0bafa50feca2f85a6ee5bfaee3d22a
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-16 07:12:42 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-16

--- src/LizeriumServer/Helpers/FileListCache.cs
+++ src/LizeriumServer/Helpers/FileListCache.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 июня 2026 07:12:48
- * Version: 1.0.80
+ * Last Updated: 16 июня 2026 07:12:27
+ * Version: 1.0.81
  */
 
 using Microsoft.Extensions.Caching.Memory;
