19eaccb0aa0a5641ec95432d9d7798e7a8b31d10
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-18 07:12:59 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-18

--- src/LizeriumServer/Middleware/DevModeMiddleware.cs
+++ src/LizeriumServer/Middleware/DevModeMiddleware.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 июня 2026 07:11:00
- * Version: 1.0.82
+ * Last Updated: 18 июня 2026 07:12:43
+ * Version: 1.0.83
  */
 
 using LizeriumUtilities.Services.DevelopService;
