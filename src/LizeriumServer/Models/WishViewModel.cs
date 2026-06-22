c71b4391c29d7245872a2d56c10c7e43e69111da
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-22 07:14:07 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-22

--- src/LizeriumServer/Models/WishViewModel.cs
+++ src/LizeriumServer/Models/WishViewModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 июня 2026 07:10:47
- * Version: 1.0.86
+ * Last Updated: 22 июня 2026 07:13:51
+ * Version: 1.0.87
  */
 
 using LizeriumUtilities.FormatsData.AppUserData;
