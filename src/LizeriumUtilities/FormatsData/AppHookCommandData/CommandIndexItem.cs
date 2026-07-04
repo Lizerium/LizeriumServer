70864baeb4519109f2475f2160b836e697346b15
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-04 08:59:53 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-04

--- src/LizeriumUtilities/FormatsData/AppHookCommandData/CommandIndexItem.cs
+++ src/LizeriumUtilities/FormatsData/AppHookCommandData/CommandIndexItem.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 03 июля 2026 08:50:41
- * Version: 1.0.97
+ * Last Updated: 04 июля 2026 08:59:42
+ * Version: 1.0.98
  */
 
 namespace LizeriumUtilities.FormatsData.AppHookCommandData;
