f6471dc7da1fa6e00669748852d73b40b40a56b9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-05 11:14:25 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-05

--- src/Api.LizeriumServer/Services/AppAuthService/IAuthService.cs
+++ src/Api.LizeriumServer/Services/AppAuthService/IAuthService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 июля 2026 08:59:42
- * Version: 1.0.98
+ * Last Updated: 05 июля 2026 11:14:13
+ * Version: 1.0.99
  */
 
 namespace Api.LizeriumServer.Services.AppAuthService;
