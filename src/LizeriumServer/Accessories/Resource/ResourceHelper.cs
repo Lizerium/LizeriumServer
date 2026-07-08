af076cdf846de907ffd7e998d106e25ab10f74f5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-08 07:48:54 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-08

--- src/LizeriumServer/Accessories/Resource/ResourceHelper.cs
+++ src/LizeriumServer/Accessories/Resource/ResourceHelper.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 июля 2026 12:41:44
- * Version: 1.0.101
+ * Last Updated: 08 июля 2026 07:48:37
+ * Version: 1.0.102
  */
 
 using Microsoft.AspNetCore.Mvc;
