215fd00568971e23d569b41b51417cb0e9b02799
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-09 07:23:26 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-09

--- src/LizeriumEmail/Services/AppEmailService/IEmailService.cs
+++ src/LizeriumEmail/Services/AppEmailService/IEmailService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 июля 2026 07:48:37
- * Version: 1.0.102
+ * Last Updated: 09 июля 2026 07:23:09
+ * Version: 1.0.103
  */
 
 using LizeriumEmail.FormatsData.AppEmailData;
