84fa3593871c8f1dfde9137dc44276ef37784df2
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-10 12:17:04 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-10

--- src/LizeriumServer/Controllers/ForumController.cs
+++ src/LizeriumServer/Controllers/ForumController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 июля 2026 07:23:09
- * Version: 1.0.103
+ * Last Updated: 10 июля 2026 12:16:48
+ * Version: 1.0.104
  */
 
 using Microsoft.AspNetCore.Mvc;
