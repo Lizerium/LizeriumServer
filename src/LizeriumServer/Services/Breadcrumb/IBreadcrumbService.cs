74bae63b91ab1178a14582da7662980f7ed4001b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-11 14:23:46 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-11

--- src/LizeriumServer/Services/Breadcrumb/IBreadcrumbService.cs
+++ src/LizeriumServer/Services/Breadcrumb/IBreadcrumbService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 июля 2026 12:16:48
- * Version: 1.0.104
+ * Last Updated: 11 июля 2026 14:23:29
+ * Version: 1.0.105
  */
 
 
