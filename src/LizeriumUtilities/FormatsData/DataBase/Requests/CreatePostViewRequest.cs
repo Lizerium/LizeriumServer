e5ef72c4877bd1e32d4ea3a000e0888b3a120585
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-15 12:14:25 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-15

--- src/LizeriumUtilities/FormatsData/DataBase/Requests/CreatePostViewRequest.cs
+++ src/LizeriumUtilities/FormatsData/DataBase/Requests/CreatePostViewRequest.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 июля 2026 07:17:23
- * Version: 1.0.108
+ * Last Updated: 15 июля 2026 12:14:10
+ * Version: 1.0.109
  */
 
 namespace LizeriumUtilities.FormatsData.DataBase.Requests;
