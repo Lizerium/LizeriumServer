15303fe13810377043702f31558b3c3bb096ed31
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-16 14:34:42 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-16

--- src/LizeriumNetSecurity/Middleware/IPBlockMiddleware.cs
+++ src/LizeriumNetSecurity/Middleware/IPBlockMiddleware.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 июля 2026 12:14:10
- * Version: 1.0.109
+ * Last Updated: 16 июля 2026 14:34:26
+ * Version: 1.0.110
  */
 
 using LizeriumNetSecurity.Services.SecurityService;
