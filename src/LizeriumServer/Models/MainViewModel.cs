978b0f3ec0cb8d608371252271ea360d65d5a8eb
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-19 10:21:46 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-19

--- src/LizeriumServer/Models/MainViewModel.cs
+++ src/LizeriumServer/Models/MainViewModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 июля 2026 07:48:27
- * Version: 1.0.112
+ * Last Updated: 19 июля 2026 10:21:30
+ * Version: 1.0.113
  */
 
 namespace LizeriumServer.Models;
