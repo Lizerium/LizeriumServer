9eb12c6e19b3b5f14796117b98848c437f6965f8
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-22 13:18:11 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-22

--- src/LizeriumDatabase/Services/AppDataBaseService/IDataBaseService.cs
+++ src/LizeriumDatabase/Services/AppDataBaseService/IDataBaseService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 июля 2026 13:52:02
- * Version: 1.0.115
+ * Last Updated: 22 июля 2026 13:17:55
+ * Version: 1.0.116
  */
 
 using LizeriumUtilities.FormatsData.AppUserData;
