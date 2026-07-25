409b8bf89df021dab888ee1cc03c633f8ee797d1
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-25 14:51:02 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-25

--- src/Api.LizeriumServer/Models/ErrorViewModel.cs
+++ src/Api.LizeriumServer/Models/ErrorViewModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 июля 2026 11:59:29
- * Version: 1.0.118
+ * Last Updated: 25 июля 2026 14:50:45
+ * Version: 1.0.119
  */
 
 namespace Api.LizeriumServer.Models
