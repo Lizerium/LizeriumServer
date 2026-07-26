58060e34a6f3a1b74bcff9aaa75d7b82e60a4373
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-26 15:33:09 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-26

--- src/LizeriumServer/Options/StoragePathsOptions.cs
+++ src/LizeriumServer/Options/StoragePathsOptions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 июля 2026 14:50:45
- * Version: 1.0.119
+ * Last Updated: 26 июля 2026 15:32:52
+ * Version: 1.0.120
  */
 
 namespace LizeriumServer.Options
