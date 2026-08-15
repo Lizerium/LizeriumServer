6cfe78c8ae040a27d9896615da018aa2cfdb00a0
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-15 07:14:48 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-15

--- src/Api.LizeriumServer/Data/AppSettings.cs
+++ src/Api.LizeriumServer/Data/AppSettings.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 августа 2026 08:37:41
- * Version: 1.0.145
+ * Last Updated: 15 августа 2026 07:14:28
+ * Version: 1.0.146
  */
 
 namespace Data;
