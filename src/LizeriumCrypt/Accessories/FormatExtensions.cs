d9a45c807a07655b76c73b482d0f4f6bb100568d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-17 07:12:23 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-17

--- src/LizeriumCrypt/Accessories/FormatExtensions.cs
+++ src/LizeriumCrypt/Accessories/FormatExtensions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 16 августа 2026 14:46:38
- * Version: 1.0.147
+ * Last Updated: 17 августа 2026 07:12:05
+ * Version: 1.0.148
  */
 
 namespace LizeriumCrypt.Accessories
