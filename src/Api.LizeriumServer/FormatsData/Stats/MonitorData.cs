d64c6dd660784a7b9c0236bbcdf109f751140448
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-20 09:58:45 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-20

--- src/Api.LizeriumServer/FormatsData/Stats/MonitorData.cs
+++ src/Api.LizeriumServer/FormatsData/Stats/MonitorData.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 19 августа 2026 10:43:54
- * Version: 1.0.150
+ * Last Updated: 20 августа 2026 09:58:25
+ * Version: 1.0.151
  */
 
 namespace Api.LizeriumServer.FormatsData.Stats;
