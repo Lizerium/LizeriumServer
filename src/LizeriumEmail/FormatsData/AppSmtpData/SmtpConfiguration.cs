c2580733029d7cf5e8924d60816ecfb6a4d8fe84
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-26 07:14:41 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-26

--- src/LizeriumEmail/FormatsData/AppSmtpData/SmtpConfiguration.cs
+++ src/LizeriumEmail/FormatsData/AppSmtpData/SmtpConfiguration.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 августа 2026 07:14:09
- * Version: 1.0.157
+ * Last Updated: 26 августа 2026 07:14:21
+ * Version: 1.0.158
  */
 
 namespace LizeriumEmail.FormatsData.AppSmtpData;
