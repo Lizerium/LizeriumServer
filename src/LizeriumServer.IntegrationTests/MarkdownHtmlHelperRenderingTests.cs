f2b51fdb2c0c08876c11b86210b1c56f1e07d8e9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-03 07:38:34 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-03

--- src/LizeriumServer.IntegrationTests/MarkdownHtmlHelperRenderingTests.cs
+++ src/LizeriumServer.IntegrationTests/MarkdownHtmlHelperRenderingTests.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 сентября 2026 07:18:07
- * Version: 1.0.164
+ * Last Updated: 03 сентября 2026 07:38:14
+ * Version: 1.0.165
  */
 
 using LizeriumServer.Helpers;
