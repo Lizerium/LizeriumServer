b12c8adfbc840d0232221561a9a19c61d2eb0f93
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-04 08:00:23 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-04

--- src/LizeriumServer/Options/SeoDomainsOptions.cs
+++ src/LizeriumServer/Options/SeoDomainsOptions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 03 сентября 2026 07:38:14
- * Version: 1.0.165
+ * Last Updated: 04 сентября 2026 08:00:04
+ * Version: 1.0.166
  */
 
 using Microsoft.AspNetCore.Http;
