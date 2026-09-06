e4fb2cd6308ea49852737e5bc43b67444d962df6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-06 11:13:45 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-06

--- src/LizeriumUtilities/FormatsData/AppEnumsData/SideError.cs
+++ src/LizeriumUtilities/FormatsData/AppEnumsData/SideError.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 сентября 2026 08:57:17
- * Version: 1.0.167
+ * Last Updated: 06 сентября 2026 11:13:26
+ * Version: 1.0.168
  */
 
 namespace LizeriumUtilities.FormatsData.AppEnumsData;
