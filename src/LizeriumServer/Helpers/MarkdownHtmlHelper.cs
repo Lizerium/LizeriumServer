06bb87c8e01ab976e4ec2549008eb5ffefa4bd78
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-24 09:42:34 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-24

--- src/LizeriumServer/Helpers/MarkdownHtmlHelper.cs
+++ src/LizeriumServer/Helpers/MarkdownHtmlHelper.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 сентября 2026 09:46:26
- * Version: 1.0.185
+ * Last Updated: 24 сентября 2026 09:42:15
+ * Version: 1.0.186
  */
 
 using HtmlAgilityPack;
